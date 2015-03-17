e2fceae637cd25b12f1d185bf0f46c303f6a80f9
petorved <d.petrov@streamcrease.com>
2015-03-17 12:21:59 +0200

    add computer modal and change some styles

--- app/js/controllers/modal.js
+++ app/js/controllers/modal.js
@@ -38,6 +38,11 @@
                 scope: $scope
             });
         };
+        $scope.openComputerModal = function () {
+            ngDialog.open({
+                template: 'partials/modals/computerModal.html.tmpl',
+                scope: $scope
+            });
+        };
     }]);
-
 })();
