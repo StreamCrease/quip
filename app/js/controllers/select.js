dda61e31d938ad0a06867c8b424c1d4c87cfd128
scdpetrov <d.petrov@streamcrease.com>
2015-03-23 14:31:27 +0200

    start another memory select implementation

--- app/js/controllers/select.js
+++ app/js/controllers/select.js
@@ -2,10 +2,26 @@
 (function () {
     'use strict';
 
-    angular.module('quipApp').controller('SelectController', ['$scope', function ($scope) {
+    angular.module('quipApp').controller('MemorySelectController', ['$scope', function ($scope) {
+        $scope.memories = [];
+
         $scope.currentSlot = 1;
         $scope.motherboard = {
             slots: 4
         }
+
+        $scope.$watch('memory', function (value) {
+            if (!!value) {
+                $scope.memories.push(value)
+                $scope.memory = null;
+            }
+        });
+
+        $scope.removeMemory = function (index) {
+            console.log(index);
+            $scope.memories.splice(index, 1);
+            console.log($scope.memories);
+        };
+
     }]);
 })();
