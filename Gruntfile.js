1796fabcdb7878b441148987a320e9d62bb9f6e4
scdpetrov <d.petrov@streamcrease.com>
2015-03-30 19:39:14 +0300

    add grunt task for build production

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function (grunt) {
         },
         htmlangular: {
             options: {
-                customattrs: ['memory-input', 'sortable', 'by', 'order', 'reverse'],
+                customattrs: ['memory-input', 'sortable', 'by', 'order', 'reverse', 'fixed-table-headers'],
                 // wrap templates as complite HTML pages
                 tmplext: 'html.tmpl',
                 reportpath: 'log/html-angular-validate-report.json'
@@ -39,6 +39,87 @@ module.exports = function (grunt) {
                 livereload: true
             }
             // tasks: ['newer:jshint', 'newer:csslint', 'newer:htmlangular']
+        },
+        env: {
+            dev: {
+                NODE_ENV: 'development'
+            },
+            prod: {
+                NODE_ENV: 'production'
+            }
+        },
+        clean: {
+            dist: 'prod/'
+        },
+        preprocess: {
+            dev: {
+                src: 'app/initial.html',
+                dest: 'app/index.html'
+            },
+            prod: {
+                src: 'app/initial.html',
+                dest: 'prod/index.html'
+            }
+        },
+        copy: {
+            fonts: {
+                cwd: 'app/bower_components/bootstrap/dist/',
+                src: 'fonts/*',
+                dest: 'prod/',
+                expand: true
+            },
+            partials: {
+                cwd: 'app/partials/',
+                src: '**/*',
+                dest: 'prod/partials/',
+                expand: true
+            },
+            packagejson: {
+                cwd: 'app/',
+                src: 'package.json',
+                dest: 'prod/',
+                expand: true
+            }
+        },
+        concat: {
+            js: {
+                src: [
+                    'app/bower_components/jquery/dist/jquery.min.js',
+                    'app/bower_components/angular/angular.min.js',
+                    'app/bower_components/angular-route/angular-route.min.js',
+                    'app/bower_components/angular-animate/angular-animate.min.js',
+                    'app/bower_components/ngDialog/js/ngDialog.min.js',
+                    'app/bower_components/StickyTableHeaders/js/jquery.stickytableheaders.min.js',
+                    'app/js/**/*.js'
+                ],
+                dest: 'prod/js/quip.js'
+            },
+            css: {
+                src: [
+                    'app/bower_components/bootstrap/dist/css/bootstrap.css',
+                    'app/bower_components/ngDialog/css/ngDialog.css',
+                    'app/bower_components/ngDialog/css/ngDialog-theme-default.css',
+                    'app/css/styles.css'
+                ],
+                dest: 'prod/css/styles.css'
+            }
+        },
+        uglify: {
+            js: {
+                src: 'prod/js/quip.js',
+                dest: 'prod/js/quip.min.js'
+            }
+        },
+        cssmin: {
+            options: {
+                compatibility: 'ie8',
+                keepSpecialComments: '*',
+                advanced: false
+            },
+            css: {
+                src: 'prod/css/styles.css',
+                dest: 'prod/css/styles.min.css'
+            }
         }
     });
 
@@ -47,7 +128,15 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-html-angular-validate');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-preprocess');
 
-    grunt.registerTask('default', ['jscs', 'jshint', 'htmlangular', 'watch']);
+    grunt.registerTask('default', ['jscs', 'jshint', 'env:dev', 'htmlangular', 'preprocess:dev', 'watch']);
+    grunt.registerTask('build', ['jscs', 'jshint', 'env:prod', 'clean', 'copy', 'concat', 'uglify', 'cssmin', 'preprocess:prod']);
 
 };
--- app/js/controllers/computers.js
+++ app/js/controllers/computers.js
@@ -9,7 +9,9 @@
             { name: 'PC02', processor: '1.3 GHz Atom E6x0', memory: '3 GB 800 MHz DDR3', graphics: 'Nvidia G-Force' },
             { name: 'MBPro', processor: '2.5 GHz Intel Core i5', memory: '6 GB 1600 MHz DDR3', graphics: 'Intel HD Graphics 4000 1024 MB' },
             { name: 'PC04', processor: '1.5 GHz Atom N400', memory: '1 GB 1600 MHz DDR2', graphics: 'ATI Radeon 2056 MB' },
-            { name: 'PC05', processor: '2.5 GHz Intel Core i5', memory: '16 GB 1600 MHz DDR3', graphics: 'Intel HD Graphics 4000 5024 MB' }
+            { name: 'PC05', processor: '2.5 GHz Intel Core i5', memory: '16 GB 1600 MHz DDR3', graphics: 'Intel HD Graphics 4000 5024 MB' },
+            { name: 'PC06', processor: '2.5 GHz Intel Core i5', memory: '16 GB 1600 MHz DDR3', graphics: 'Intel HD Graphics 4000 5024 MB' },
+            { name: 'PC07', processor: '2.5 GHz Intel Core i5', memory: '16 GB 1600 MHz DDR3', graphics: 'Intel HD Graphics 4000 5024 MB' }
         ];
 
     }]);
