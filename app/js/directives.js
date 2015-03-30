9508935fe1765937598f28831fcfb6e3ed6f570d
scdpetrov <d.petrov@streamcrease.com>
2015-03-30 16:19:36 +0300

    add jQuery and StickyTableHeaders plugin for scrollable table with fixed thead

--- app/js/directives.js
+++ app/js/directives.js
@@ -62,4 +62,16 @@
         }
     });
 
+    angular.module('quipApp').directive('fixedTableHeaders', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                $timeout(function () {
+                    var container = element.parentsUntil(attrs.fixedTableHeaders);
+                    element.stickyTableHeaders({ scrollableArea: container, 'fixedOffset': 1 });
+                }, 0);
+            }
+        }
+    }]);
+
 })();
